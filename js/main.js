dc7b81c1a19a42fa0769837995851568144db431
Aglailson <jaglailson1@gmail.com>
2021-05-06 19:20:14 -0300

    testar efeitos menu no mobile

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 window.onload = function(){
+    (function ajustes(){
+        //espaçando a logo das midias do menu
+        let alturaContentLogo = document.getElementById('content-logo-btn-menu').offsetHeight
+        let menu = document.querySelector('.menu')
+        menu.style.paddingTop = alturaContentLogo+"px"
+
+        console.log(menu)
+
+    }())
     //btn-menu
     var btnMenu = $('.btn-menu')
 
@@ -20,9 +29,20 @@ window.onload = function(){
         if(contentMenu.hasClass('content-menu-open')){
 
             contentMenu.removeClass('content-menu-open')
-
+            logoClaro()
         }else{
             contentMenu.addClass('content-menu-open')
+            logoEscuro()
         }
     }
+    //Muda cor logo
+    function logoEscuro(){
+        let logoSpan = $('.logo #logo-span')
+        logoSpan.css("color","#333")
+    }
+    function logoClaro(){
+        let logoSpan = $('.logo #logo-span')
+        logoSpan.css("color","#fff")
+    }
+    
 }
