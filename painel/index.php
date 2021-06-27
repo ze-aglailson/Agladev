2ed13cf87ded87c01fa4fa4a0f9e2e347d49a9e2
Aglailson <jaglailson1@gmail.com>
2021-06-27 17:02:19 -0300

    atualizando pequenos bugs

--- painel/index.php
+++ painel/index.php
@@ -7,13 +7,23 @@
 
     }
 
-    echo "<a href='../?pagina=pessoa&metodo=logout'>Sair</a>";
-    echo "Seja bem vindo ao painel ". $_SESSION['nome'];
-
     $pessoa = new Pessoa;
-    
-    
-  
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <button><a href="../?pagina=pessoa&metodo=logout">Sair</a></button><br>
+    <h2>Seja bem vindo ao painel<?php echo " <a href='#'>".$_SESSION['nome'] ."</a>"?></h2>
+    
+</body>
+</html>
+
+<!-- CRIAR UM TEMPLATE PARA O PAINEL DE CONTROLE-->
